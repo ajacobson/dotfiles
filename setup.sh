b1d0180295929093cf3c6559b3d6d22172afefda
Art Jacobson <art.jacobson@gmail.com>
2014-10-20 16:31:05 -0400

    Don't follow directory symlinks when symlinking directories

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )";
 
 ln -sf "$DIR/etc/zshrc" "$HOME/.zshrc"
-ln -sf "$DIR/etc/zsh" "$HOME/.zsh"
+ln -sfh "$DIR/etc/zsh" "$HOME/.zsh"
 ln -sf "$DIR/etc/screenrc" "$HOME/.screenrc"
 ln -sf "$DIR/etc/gitconfig" "$HOME/.gitconfig"
 ln -sf "$DIR/etc/gitignore_global" "$HOME/.gitignore_global"
 ln -sf "$DIR/etc/agignore" "$HOME/.agignore"
 ln -sf "$DIR/etc/emacs" "$HOME/.emacs"
-ln -sf "$DIR/etc/emacs.d" "$HOME/.emacs.d"
+ln -sfh "$DIR/etc/emacs.d" "$HOME/.emacs.d"
 
 if [ -e "/usr/bin/sw_vers" ]; then
     exec osx/set_defaults
