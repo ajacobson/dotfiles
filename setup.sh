12b6999b6163bc665100035afc4f63faa495e782
Art Jacobson <art.jacobson@gmail.com>
2014-10-20 16:22:36 -0400

    Updates directory structure

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 DIR="$( cd "$( dirname "$0" )" && pwd )";
 
-ln -sf "$DIR/zshrc" "$HOME/.zshrc"
-ln -sf "$DIR/zsh" "$HOME/.zsh"
-ln -sf "$DIR/screenrc" "$HOME/.screenrc"
-ln -sf "$DIR/gitconfig" "$HOME/.gitconfig"
-ln -sf "$DIR/gitignore_global" "$HOME/.gitignore_global"
-ln -sf "$DIR/agignore" "$HOME/.agignore"
-ln -sf "$DIR/emacs" "$HOME/.emacs"
-ln -sf "$DIR/emacs.d" "$HOME/.emacs.d"
+ln -sf "$DIR/etc/zshrc" "$HOME/.zshrc"
+ln -sf "$DIR/etc/zsh" "$HOME/.zsh"
+ln -sf "$DIR/etc/screenrc" "$HOME/.screenrc"
+ln -sf "$DIR/etc/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DIR/etc/gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$DIR/etc/agignore" "$HOME/.agignore"
+ln -sf "$DIR/etc/emacs" "$HOME/.emacs"
+ln -sf "$DIR/etc/emacs.d" "$HOME/.emacs.d"
+
+if [ -e "/usr/bin/sw_vers" ]; then
+    exec osx/set_defaults
+fi
